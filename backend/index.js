b6a4d27d389962a9fe1160a34261527c1f4aaca7
enriquegiottonini <enriquegiottonini@gmail.com>
2022-07-09 17:52:06 -0700

    new cors config

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,25 @@ mongoose
 
     const app = express();
 
-    app.use(cors());
+    // app.use(cors());
+
+    //Cors Configuration - Start
+    app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+      );
+      if (req.method === "OPTIONS") {
+        res.header(
+          "Access-Control-Allow-Methods",
+          "POST, PUT, PATCH, GET, DELETE"
+        );
+        return res.status(200).json({});
+      }
+      next();
+    });
+    //Cors Configuration - End
     app.use(express.json());
     app.use(express.static("build"));
     app.use("/api", routes);
