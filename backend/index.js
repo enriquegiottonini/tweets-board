c467742a29bf600aade24049c406568a06e52b45
enriquegiottonini <enriquegiottonini@gmail.com>
2022-07-09 17:31:17 -0700

    problems with cors and connection topology

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,16 @@ require("dotenv").config({ path: join(__dirname, "..", ".env") });
 
 // Setting up a default mongoose connection
 mongoose
-  .connect(process.env.ATLAS_CONNECT_URL)
+  .connect(process.env.ATLAS_CONNECT_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to DB.");
 
     const app = express();
-    app.use(cors());
+
+    //app.use(cors());
     app.use(express.json());
     app.use(express.static("build"));
     app.use("/api", routes);
